a40110ac53d4a4650c93e5de6c74610a97c062d3
diegoversiani <diegoversiani@gmail.com>
2016-07-09 11:30:42 -0300

    Add fonts to precompile

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module FlapperNewsRails
     config.active_record.raise_in_transactional_callbacks = true
 
     config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
   end
 end
